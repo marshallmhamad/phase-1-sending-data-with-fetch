4cd00ec853d49083ab263ee56cb70d04c95cb767
marshall <mhamad.marshall@gmail.com>
2022-11-07 10:53:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+function submitData(name,email){
+
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+
+
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+
+     return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+            const p = document.createElement('p')
+           return document.querySelector('body').appendChild(p).textContent = object.id
+         // console.log(object);
+        })
+        .catch(function (error) {
+            const p = document.createElement('p')
+            return document.querySelector('body').appendChild(p).textContent = error
+        //    alert (error.message);
+        //   console.log(error.message);
+        });
+
+
+
+
+
+
+}
